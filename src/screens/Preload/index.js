7a463be4299f8e411d80061581e1bde6072ef176
Leonardo Pilatti <leonardopilatti14@gmail.com>
2021-10-04 18:48:14 -0300

    criando o preload e direcionando para o signin caso não tenha o token salvo no asyncStorage

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles';
+import {Container, LoadingIcon} from './styles';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useNavigation} from '@react-navigation/native';
+
+import BarberLogo from '../../assets/barber.svg';
 
 const index = () => {
+  const navigation = useNavigation();
+
+  React.useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem('@token');
+      if (token) {
+        /// se tiver token, ou seja, caso ele seja diferente de null;
+        //validar o token
+      } else {
+        navigation.navigate('SignIn');
+      }
+    };
+    checkToken();
+  }, []);
+
   return (
     <Container>
-      <Text>Preload</Text>
+      <BarberLogo width="100%" height="160" />
+      <LoadingIcon size="large" color="#fff" />
     </Container>
   );
 };
--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -9,7 +9,12 @@ const MainStack = () => {
   const Stack = createStackNavigator();
 
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      initialRouteName="Preload" /// este initialRouteName é para ele garantir que a página inicial sempre será a preload;
+      screenOptions={{
+        headerShown: false,
+      }} /* /// headershown false para ele subir o cabeçalho, sumir de todas as telas; */
+    >
       <Stack.Screen name="Preload" component={Preload} />
       <Stack.Screen name="SignIn" component={SignIn} />
       <Stack.Screen name="SignUp" component={SignUp} />
