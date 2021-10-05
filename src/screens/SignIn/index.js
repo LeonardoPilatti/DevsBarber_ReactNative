8902531d849debdc795dd5e98e667e0787b52804
Leonardo Pilatti <leonardopilatti14@gmail.com>
2021-10-04 21:31:54 -0300

    criando o layout da pagina de login

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import {Text} from 'react-native';
-import {Container} from './styles';
+import {
+  Container,
+  InputArea,
+  CustomButton,
+  CustomButtonText,
+  SignMessageButton,
+  SignMessageButtonText,
+  SignMessageButtonTextBold,
+} from './styles';
+
+import BarberLogo from '../../assets/barber.svg';
+import SignInInput from '../../components/SignInInput';
+
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
 
 const index = () => {
   return (
     <Container>
-      <Text>SignIn</Text>
+      <BarberLogo width="100%" height="160" />
+
+      <InputArea>
+        <SignInInput IconSvg={EmailIcon} placeholder="Digite seu e-mail" />
+        <SignInInput IconSvg={LockIcon} placeholder="Digite sua senha" />
+
+        <CustomButton>
+          <CustomButtonText>LOGIN</CustomButtonText>
+        </CustomButton>
+      </InputArea>
+
+      <SignMessageButton>
+        <SignMessageButtonText>
+          Ainda não possui uma conta?
+        </SignMessageButtonText>
+        <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
+      </SignMessageButton>
     </Container>
   );
 };
